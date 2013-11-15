8c363629cdf43ffd318f7f990f1c3d06cbb0fb61
Aaron Parecki <aaron@parecki.com>
2013-11-15 14:45:13 -0800

    update to jsonatra 1.0.3 and refactor to use .blank? method for error checking

--- app.rb
+++ app.rb
@@ -15,13 +15,15 @@ class App < Jsonatra::Base
   end
 
   post '/report' do
-    param_error :date, 'missing', 'date parameter required' unless params[:date]
+    param_error :date, 'missing', 'date parameter required' if params[:date].blank?
     param_error :date, 'invalid', 'date parameter should look like YYYYMMDD' unless params[:date] && params[:date].match(/^\d{8}$/)
-    param_error :client_id, 'missing', 'client_id parameter required' unless params[:client_id]
-    param_error :key, 'missing', 'key parameter required' if params[:key] == nil or params[:key].empty?
-    param_error :value, 'missing', 'value parameter required' if params[:value] == nil or params[:value].empty?
-    param_error :number, 'missing', 'number parameter required' if params[:number] == nil or params[:number].empty?
+    param_error :client_id, 'missing', 'client_id parameter required' if params[:client_id].blank?
+    param_error :key, 'missing', 'key parameter required' if params[:key].blank?
+    param_error :value, 'missing', 'value parameter required' if params[:value].blank?
+    param_error :number, 'missing', 'number parameter required' if params[:number].blank?
     param_error :number, 'invalid', 'number must be an integer' unless params[:number] && params[:number].match(/^[0-9]+$/)
+
+    halt if response.error?
 
     DB.exec 'BEGIN'
     DB.exec_prepared 'updatestat', [params[:number], params[:client_id], params[:date], params[:key], params[:value]]
