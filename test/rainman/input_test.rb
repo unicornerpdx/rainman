bf2e89b5c9371471dabf4767fb7588dc103ce421
Aaron Parecki <aaron@parecki.com>
2013-12-30 16:55:53 -0800

    add some basic tests for parameter validation

--- test/rainman/input_test.rb
+++ test/rainman/input_test.rb
@@ -30,4 +30,51 @@ class AppTest < MiniTest::Unit::TestCase
     assert_equal [], parameters
   end
 
+  def test_report_rejects_invalid_date
+    post '/report', { date: 'FOO' }
+    response = JSON.parse last_response.body
+    assert_equal 'invalid', response['error']['parameters']['date'][0]['type']
+
+    post '/report', { date: '20131201' }
+    response = JSON.parse last_response.body
+    assert_equal 'invalid', response['error']['parameters']['date'][0]['type']
+  end
+
+  def test_report_accepts_valid_date
+    post '/report', { date: '2013-12-01' }
+    response = JSON.parse last_response.body
+    assert_nil response['error']['parameters']['date']
+  end
+
+  def test_report_accepts_valid_client_id
+    post '/report', { client_id: '100000' }
+    response = JSON.parse last_response.body
+    assert_nil response['error']['parameters']['client_id']
+  end
+
+  def test_report_accepts_valid_key
+    post '/report', { key: 'version' }
+    response = JSON.parse last_response.body
+    assert_nil response['error']['parameters']['key']
+  end
+
+  def test_report_accepts_valid_value
+    post '/report', { value: '1.0' }
+    response = JSON.parse last_response.body
+    assert_nil response['error']['parameters']['value']
+  end
+
+  def test_report_accepts_valid_number
+    post '/report', { number: 100 }
+    response = JSON.parse last_response.body
+    assert_nil response['error']['parameters']['number']
+  end
+
+  def test_report_rejects_invalid_number
+    post '/report', { number: 'FOO' }
+    response = JSON.parse last_response.body
+    assert_equal 'invalid', response['error']['parameters']['number'][0]['type']
+    assert_equal 'number must be an integer', response['error']['parameters']['number'][0]['message']
+  end
+
 end
