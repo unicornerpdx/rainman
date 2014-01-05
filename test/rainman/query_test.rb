ab2acb8d857b966928a9b1c489d978d4e5a05507
Aaron Parecki <aaron@parecki.com>
2014-01-04 17:28:20 -0800

    generates some sample data for tests

--- test/rainman/query_test.rb
+++ test/rainman/query_test.rb
@@ -10,24 +10,48 @@ class AppTest < MiniTest::Unit::TestCase
 
   def setup
     DB.exec 'DELETE FROM stats'
+
     # Insert a bunch of test data to query
 
-    post '/report', {
-      group_id: 'group-a',
-      client_id: 'client-1',
-      date: '2014-01-01',
-      key: 'version',
-      value: '1.0',
-      number: 2
-    }
-    post '/report', {
-      group_id: 'group-a',
-      client_id: 'client-1',
-      date: '2014-01-02',
-      key: 'version',
-      value: '1.0',
-      number: 2
-    }
+    (0..30).each do |i|
+      post '/report', {
+        group_id: 'group-a',
+        client_id: 'client-1',
+        date: (DateTime.parse('2014-01-01')+i).strftime('%Y-%m-%d'),
+        key: 'version',
+        value: '1.0',
+        number: i
+      }
+    end
+
+    ['b','c','d','e'].each do |g|
+      (1..4).each do |c|
+        post '/report', {
+          group_id: "group-#{g}",
+          client_id: "client-#{c}",
+          date: '2014-01-01',
+          key: 'name',
+          value: 'test',
+          number: 5
+        }
+        post '/report', {
+          group_id: "group-#{g}",
+          client_id: "client-#{c}",
+          date: '2014-01-01',
+          key: 'version',
+          value: '1.0',
+          number: 2
+        }
+        post '/report', {
+          group_id: "group-#{g}",
+          client_id: "client-#{c}",
+          date: '2014-01-01',
+          key: 'version',
+          value: '1.1',
+          number: 3
+        }
+      end
+    end
 
   end
 
